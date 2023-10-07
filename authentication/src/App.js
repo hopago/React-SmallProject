e93794aad85480a964cab9e92ce84c6b0194d1c3
Hopago <132269386+hopago@users.noreply.github.com>
2023-10-07 16:45:07 +0900

    Handle authentication in client side

---
+++ authentication/src/App.js
@@ -0,0 +1,14 @@
+import Login from "./Login";
+import Register from "./Register";
+
+
+function App() {
+  return (
+    <>
+      {/* <Register /> */}
+      <Login />
+    </>
+  );
+}
+
+export default App;
